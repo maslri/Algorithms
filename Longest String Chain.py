933b03aef34d6e9a9a9f00c4a5cc38f5ef48d74c
maslri <ma.slri2128@gmail.com>
2023-09-24 15:44:21 +0330

    changed way of solve problem

--- Longest String Chain.py
+++ Longest String Chain.py
@@ -1,50 +1,91 @@
 # https://leetcode.com/problems/longest-string-chain/description/
 
 from typing import List
+from numpy import arange
 
-class Solution:
+class Solution :
 
-    count = 0
-    
     def longestStrChain(self, words: List[str]) -> int:
-        if len(words) == 1 :
-            return 1
-              
-        min_list = [s for s in words if len(s) == len(min(words, key=len))]
-        words = [x for x in words if x not in min_list]
-
-        for min_l in min_list :
-            self.change_list(min_l, words)
-        return self.count
-
-    def change_list(self, min_l : str, words : List[str]) -> None :
-        if not words :
-            return self.count
         
-        min_list = [i for i in words if len(i) == len(min_l) + 1]
-
-        for min_ll in min_list :
-            if self.isPredecessor(min_l, min_ll) :
-                self.count += 1
-                self.change_list(min_ll, words)
-        else :
-            words = [x for x in words if x not in min_list]
+        word_set = set(words)
+        max_chain = 1
+        for word in words:
+            max_chain = max(max_chain, self.findLongestChain(word, word_set))
+        return max_chain
     
-    def isPredecessor(self, a : str, b : str) -> bool :
-        valid = 0
-        for i in a :
-            check = b.find(i)
-            if valid > check :
-                return False
-            else :
-                valid = check
-                del(check)
-        else :
+    def isPredecessor(self, a: str, b: str) -> bool:
+            valid = 0
+            for i in a:
+                check = b.find(i, valid)
+                if valid > check:
+                    return False
+                valid = check + 1
             return True
+    
+    def findLongestChain(self, word: str, word_set: set) -> int:
+            max_chain = 1
+            for i in range(len(word)):
+                new_word = word[:i] + word[i+1:]
+                if new_word in word_set:
+                    max_chain = max(max_chain, 1 + self.findLongestChain(new_word, word_set))
+            return max_chain
 
+class Solution2 :
 
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key=len)  # Sort the words by length
+        dp = {}  # Dictionary to store the longest chain length for each word       
+        max_chain = 1  # Minimum chain length is 1 (the word itself)
+        
+        for word in words:
+            max_chain_for_word = 1
+            for i in range(len(word)):
+                prev_word = word[:i] + word[i+1:]  # Remove one character
+                if prev_word in dp:
+                    max_chain_for_word = max(max_chain_for_word, dp[prev_word] + 1)
+            dp[word] = max_chain_for_word
+            max_chain = max(max_chain, max_chain_for_word)
+        
+        return max_chain
+
+class Solution3 :
+     def longestStrChain(self, words):
+        dp = {}
+        for w in sorted(words, key=len):
+            dp[w] = max(dp.get(w[:i] + w[i + 1:], 0) + 1 for i in arange(len(w)))
+        return max(dp.values())
+
+class Solution4 :
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key=len)
+        dp = {}
+        max_chain = 0
+        for word in words:
+            dp[word] = 1
+            for i in range(len(word)):
+                prev_word = word[:i] + word[i+1:]
+                if prev_word in dp:
+                    dp[word] = max(dp[word], dp[prev_word] + 1)
+            max_chain = max(max_chain, dp[word])
+        return max_chain
+
+# Test cases
 a = Solution()
-print(a.longestStrChain(["a","b","ba","bca","bda","bdca"]))
-#print(a.longestStrChain(["xbc","pcxbcf","xb","cxbc","pcxbc"]))
-#print(a.longestStrChain(["abcd","dbqca"]))
+b = Solution2()
+c = Solution3()
+d = Solution4()
+print(a.longestStrChain(["a","b","ba","bca","bda","bdca"]))  # Output: 4
+print(b.longestStrChain(["a","b","ba","bca","bda","bdca"]))  # Output: 4
+print(c.longestStrChain(["a","b","ba","bca","bda","bdca"]))  # Output: 4
+print(d.longestStrChain(["a","b","ba","bca","bda","bdca"]))  # Output: 4
+print("------------------------------------")
+print(a.longestStrChain(["xbc","pcxbcf","xb","cxbc","pcxbc"]))  # Output: 5
+print(b.longestStrChain(["xbc","pcxbcf","xb","cxbc","pcxbc"]))  # Output: 5
+print(c.longestStrChain(["xbc","pcxbcf","xb","cxbc","pcxbc"]))  # Output: 5
+print(d.longestStrChain(["xbc","pcxbcf","xb","cxbc","pcxbc"]))  # Output: 5
+print("------------------------------------")
+print(a.longestStrChain(["abcd","dbqca"]))  # Output: 1
+print(b.longestStrChain(["abcd","dbqca"]))  # Output: 1
+print(c.longestStrChain(["abcd","dbqca"]))  # Output: 1
+print(d.longestStrChain(["abcd","dbqca"]))  # Output: 1
 
